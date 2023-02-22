36ba08aaa8a3d107b1488b1c79110238402d3f8a
Rayan Tejani <rayantejani@utexas.edu>
2023-02-21 19:26:51 -0600

    Basic sign in page

---
+++ teamproject/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInPage from './pages/SignInPage';
+
+function App() {
+  return (
+    <div>
+      <SignInPage />
+    </div>
+  );
+}
+
+export default App;
