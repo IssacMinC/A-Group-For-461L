88962ac90b8db4c2be69923fbf39a5015faf6fbd
Ranit Gupta <ranitgupta06@gmail.com>
2023-03-28 20:36:59 -0500

    Fixed function naming and altered for Mongo structure

--- backend/projects.py
+++ backend/projects.py
@@ -1,33 +1,49 @@
 import pymongo
 
-clientLink = ""
+clientLink = "mongodb+srv://test:DMfmcal3RyXG9gLl@cluster1.wjept6s.mongodb.net/?retryWrites=true&w=majority"
 
 class projects():
 
     #Create a new Project object in MongoDB based onlyy on id
-    def createProject(id):
+    def create(id):
         client = pymongo.MongoClient(clientLink)
         db = client.Project
         collection = db.Projects
         proj = {"projectID": id,
-                "HW1": 0,
-                "HW2": 0}
+                "HWSet1": 0,
+                "HWSet2": 0}
         collection.insert_one(proj)
         client.close
 
     #Create a new Project object in MongoDB with hardware set info
-    def createProject(id, hw1, hw2):
+    def create(id, hw1, hw2):
         client = pymongo.MongoClient(clientLink)
         db = client.Project
         collection = db.Projects
         proj = {"projectID": id,
-                "HW1": hw1,
-                "HW2": hw2}
+                "HWSet1": hw1,
+                "HWSet2": hw2}
         collection.insert_one(proj)
         client.close
 
-    #Search for a project based on ID
-    def queryProject(id):
+    #Update Project HW Set 1 usage
+    def setHW1(id, num):
+        client = pymongo.MongoClient(clientLink)
+        db = client.Project
+        collection = db.Projects
+        collection.find_one({'projectID': id}, {"$set": {'HWSet1': num}})
+        client.close
+
+    #Update Project HW Set 2 usage
+    def setHW2(id, num):
+        client = pymongo.MongoClient(clientLink)
+        db = client.Project
+        collection = db.Projects
+        collection.find_one({'projectID': id}, {"$set": {'HWSet2': num}})
+        client.close
+
+    #get a project based on ID
+    def get(id):
         client = pymongo.MongoClient(clientLink)
         db = client.Project
         collection = db.Projects
@@ -35,21 +51,24 @@ class projects():
         client.close
         return result
     
-    #Update Project HW Set 1 usage
-    def createProject(id, num):
+    #get project HW1 usage based on ID
+    def getHW1(id):
         client = pymongo.MongoClient(clientLink)
         db = client.Project
         collection = db.Projects
-        collection.find_one({'projectID': id}, {"$set": {'HW1': num}})
+        result = collection.find_one({'projectID': id})
         client.close
+        return result['HWSet1']
+    
+    #get project HW2 usage based on ID
+    def getHW2(id):
+        client = pymongo.MongoClient(clientLink)
+        db = client.Project
+        collection = db.Projects
+        result = collection.find_one({'projectID': id})
+        client.close
+        return result['HWSet2']
 
-    #Update Project HW Set 2 usage
-    def createProject(id, num):
-        client = pymongo.MongoClient(clientLink)
-        db = client.Project
-        collection = db.Projects
-        collection.find_one({'projectID': id}, {"$set": {'HW2': num}})
-        client.close
     
 # Test 1: Create a test Project and Search for it
 #   projects.createProject("test Name", "test ID", "test Description")
