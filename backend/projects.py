3d0e390945c5073477668984ca3ba9fc2b1c383c
Ranit Gupta <ranitgupta06@gmail.com>
2023-03-28 20:22:47 -0500

    Updated projects.py to reflect MongoDB structure

--- backend/projects.py
+++ backend/projects.py
@@ -1,54 +1,54 @@
 import pymongo
 
-clientLink = "mongodb+srv://test:2123@cluster1.wjept6s.mongodb.net/?retryWrites=true&w=majority"
+clientLink = ""
 
 class projects():
 
-    #Create a new Project object in MongoDB
+    #Create a new Project object in MongoDB based onlyy on id
     def createProject(id):
         client = pymongo.MongoClient(clientLink)
-        db = client.Projects
-        collection = db.Project1
-        proj = {"Name": "",
-                "ID": id,
-                "Description": ""}
+        db = client.Project
+        collection = db.Projects
+        proj = {"projectID": id,
+                "HW1": 0,
+                "HW2": 0}
         collection.insert_one(proj)
         client.close
 
-    #Create a new Project object in MongoDB with name and description
-    def createProject(name, id, description):
+    #Create a new Project object in MongoDB with hardware set info
+    def createProject(id, hw1, hw2):
         client = pymongo.MongoClient(clientLink)
-        db = client.Projects
-        collection = db.Project1
-        proj = {"Name": name,
-                "ID": id,
-                "Description": description}
+        db = client.Project
+        collection = db.Projects
+        proj = {"projectID": id,
+                "HW1": hw1,
+                "HW2": hw2}
         collection.insert_one(proj)
         client.close
 
     #Search for a project based on ID
     def queryProject(id):
         client = pymongo.MongoClient(clientLink)
-        db = client.Projects
-        collection = db.Project1
-        result = collection.find_one({'ID': id})
+        db = client.Project
+        collection = db.Projects
+        result = collection.find_one({'projectID': id})
         client.close
         return result
     
-    #Update Project Name
-    def createProject(id, name):
+    #Update Project HW Set 1 usage
+    def createProject(id, num):
         client = pymongo.MongoClient(clientLink)
-        db = client.Projects
-        collection = db.Project1
-        collection.find_one({'ID': id}, {"$set": {'name': name}})
+        db = client.Project
+        collection = db.Projects
+        collection.find_one({'projectID': id}, {"$set": {'HW1': num}})
         client.close
 
-    #Update Project Description
-    def createProject(id, description):
+    #Update Project HW Set 2 usage
+    def createProject(id, num):
         client = pymongo.MongoClient(clientLink)
-        db = client.Projects
-        collection = db.Project1
-        collection.find_one({'ID': id}, {"$set": {'description': description}})
+        db = client.Project
+        collection = db.Projects
+        collection.find_one({'projectID': id}, {"$set": {'HW2': num}})
         client.close
     
 # Test 1: Create a test Project and Search for it
