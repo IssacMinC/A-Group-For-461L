6389285c8c43b6bcff4db80969dde3b88d0ef34d
Mingi Hong <keuang@wireless-10-145-57-51.public.utexas.edu>
2023-03-29 10:21:43 -0500

    fixed projectDB

--- backend/projectDB.py
+++ backend/projectDB.py
@@ -3,70 +3,117 @@ import certifi
 from flask import jsonify
 ca = certifi.where()
 
-def createProject(projectID, project_name, description, date):
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    doc = {"Name":project_name,"ID":projectID, "Description":description, "CreationDate":date, "HardWareHistory": ""}
-    col.insert_one(doc)
-    return
+clientLink = "insert link here"
+def createProject(projectID):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["Project"]
+    col = db["Projects"]
+    check = col.find_one({"projectID":projectID})
+    if check == None:
+        doc = {"projectID":projectID,"HWSet1":0, "HWSet2":0}
+        col.insert_one(doc)
+        client.close()
+        return "created project"
+    else:
+        client.close()
+        return "already existing project"
 
-def getProjectName(projectID)
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    dicti = col.find_one({"ID":projectID}, {"_id":0, "Description":0,"CreationDate":0,"HardWareHistory":0})
-    projName = dicti["Name"]
-    return str(projName)
+# def deleteProject(projectID):
+#     client = MongoClient(clientLink, tlsCAfile=ca)
+#     db = client["Project"]
+#     col = db["Projects"]
+#     col.delete_one({"projectID":projectID})
+#     dbUser = client["User"]
+#     colUser = dbUser["Users"]
+#     doc = colUser.find_one({})
 
 def getProjectID(userID):
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Users"]
-    col = db["users"]
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["User"]
+    col = db["Users"]
     dicti = col.find_one({"UserID":userID}, {"_id": 0, "Password": 0})
-    projID = dicti["Projects"]
-    return str(projID)
+    projID = dicti["ProjectID"]
+    return projID
 
-def getDescription(projectID)
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    dicti = col.find_one({"ID":projectID}, {"_id":0,"Name":0,"CreationDate":0 ,"HardWareHistory":0})
-    desc = dicti["Description"]
-    return str(desc)
+def getHW1(projectID):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["Project"]
+    col = db["Projects"]
+    dicti = col.find_one({"projectID":projectID}, {"_id":0,"HWSet2":0})
+    hwSet1 = dicti["HWSet1"]
+    client.close()
+    return int(hwSet1)
 
-def getCreationDate(projectID)
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    dicti = col.find_one({"ID":projectID}, {"_id":0,"Name":0, "Description":0,"HardWareHistory":0})
-    createDate = dicti["CreationDate"]
-    return str(createDate)
+def getHW2(projectID):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["Project"]
+    col = db["Projects"]
+    dicti = col.find_one({"projectID":projectID}, {"_id":0,"HWSet1":0})
+    hwSet2 = dicti["HWSet2"]
+    client.close()
+    return int(hwSet2)
+
+def getCapacity(hwSet):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["HardwareSet"]
+    col = db["Hardware"]
+    dicti = col.find_one({"Name":hwSet},{"_id":0})
+    capacity = dicti["Capacity"]
+    client.close()
+    return int(capacity)
+
+def getAvail(hwSet):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["HardwareSet"]
+    col = db["Hardware"]
+    dicti = col.find_one({"Name":hwSet},{"_id":0})
+    avail = dicti["Capacity"]
+    client.close()
+    return int(avail)
 
 
-def getHWSets(projectID):
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    dicti = col.find_one({"ID":projectID}, {"_id":0,"Name":0, "Description":0,"CreationDate":0})
-    HW = dicti["HardWareHistory"]
-    return str(HW)
+def checkIn(projectID, hwSet, qty):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["HardwareSet"]
+    col = db["Hardware"]
+    dicti = col.find_one({"Name":hwSet}, {"_id":0})
+    avail = dicti["Availability"]
+    dbProject = client["Project"]
+    colProject = dbProject["Projects"]
+    dictProject = colProject.find_one({"projectID":projectID},{"_id":0})
+    hwSetQty = dictProject[hwSet]
+    if qty < 0:
+        client.close()
+        return "error: qty has to be greater than 0"
+    elif hwSetQty < qty:
+        client.close()
+        return "error: qty is bigger than what is checked out"
+    else:
+        col.update_one({"Name":hwSet},{"$set": {"Availability":(avail+qty)}})
+        colProject.update_one({"projectID":projectID},{"$set": {hwSet:(hwSetQty-qty)}})
+        client.close()
+        return "check in sucessful"
 
-def deleteProject(projectID):
-    client = MongoClient("mongodb+srv://rayantejani:abcd@cluster0.quex5af.mongodb.net/?retryWrites=true&w=majority", tlsCAfile=ca)
-    db = client["Projects"]
-    col = db["Project1"]
-    col.delete_one({"ID":projectID})
-
-    #Update
-    dbUsers = client["Users"]
-    colUsers = dbUsers["users"]
-    doc = colUsers.find_one({})
-    projIDList = doc["Projects"]
-    newProjIDList = ""
-    str_list = projIDList.split(projectID)
-    for element in str_list:
-        newProjIDList+=element
-    colUsers.update_one({"Projects":projIDList}, { "$set": { "Projects": newProjIDList } })
+def checkOut(projectID, hwSet, qty):
+    client = MongoClient(clientLink, tlsCAfile=ca)
+    db = client["HardwareSet"]
+    col = db["Hardware"]
+    dicti = col.find_one({"Name":hwSet}, {"_id":0})
+    avail = dicti["Availability"]
+    dbProject = client["Project"]
+    colProject = dbProject["Projects"]
+    dictProject = colProject.find_one({"projectID":projectID},{"_id":0})
+    hwSetQty = dictProject[hwSet]
+    if qty < 0:
+        client.close()
+        return "error: qty has to be greater than 0"
+    elif qty > avail:
+        client.close()
+        return "error: requested more qty than available"
+    else:
+        col.update_one({"Name":hwSet},{"$set": {"Availability":(avail-qty)}})
+        colProject.update_one({"projectID":projectID},{"$set": {hwSet:(hwSetQty+qty)}})
+        client.close()
+        return "check out sucessful"
 
 
